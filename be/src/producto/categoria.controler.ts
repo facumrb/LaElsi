54c0bcbd16cd970a687ab078555d7a76361981bd
Facundo <facundomrb@gmail.com>
2026-01-22 14:11:33 -0300

    Just a comment

--- be/src/producto/categoria.controler.ts
+++ be/src/producto/categoria.controler.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { Categoria } from './categoria.entity.js';
 import { orm } from '../shared/db/orm.js';
 
-const em = orm.em;
+const em = orm.em; // Entity Manager
 
 function sanitizeCategoriaInput(req: Request, res: Response, next: NextFunction) {
   req.body.sanitizedInput = {
